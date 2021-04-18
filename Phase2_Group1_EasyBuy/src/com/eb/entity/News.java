2fd378a31d889ddedf11fd409109f3277003acae
ztulnnx <jiabao52002443@163.com>
2021-04-18 11:34:13 +0800

    News

---
+++ Phase2_Group1_EasyBuy/src/com/eb/entity/News.java
@@ -0,0 +1,62 @@
+package com.eb.entity;
+
+
+//ÐÂÎÅs
+public class News {
+	private int nid;
+	private String uid;
+	private String nTitle;
+	
+	private String nContent;
+
+	public int getNid() {
+		return nid;
+	}
+
+	public void setNid(int nid) {
+		this.nid = nid;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getnTitle() {
+		return nTitle;
+	}
+
+	public void setnTitle(String nTitle) {
+		this.nTitle = nTitle;
+	}
+
+	public String getnContent() {
+		return nContent;
+	}
+
+	public void setnContent(String nContent) {
+		this.nContent = nContent;
+	}
+
+	public News(int nid, String uid, String nTitle, String nContent) {
+		super();
+		this.nid = nid;
+		this.uid = uid;
+		this.nTitle = nTitle;
+		this.nContent = nContent;
+	}
+
+	public News(String uid, String nTitle, String nContent) {
+		super();
+		// TODO Auto-generated constructor stub
+		this.uid = uid;
+		this.nTitle = nTitle;
+		this.nContent = nContent;
+	}
+	
+	
+	
+}	
