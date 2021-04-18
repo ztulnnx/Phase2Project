aa7618d3b17ab5273b351695ee17d3241c26279c
ztulnnx <jiabao52002443@163.com>
2021-04-18 11:39:59 +0800

    update

---
+++ Phase2_Group1_EasyBuy/src/com/eb/entity/ReceiverAddress.java
@@ -0,0 +1,21 @@
+package com.eb.entity;
+
+public class ReceiverAddress {
+	private int rid;
+	private String  receiverAddress;
+	
+	public int getRid() {
+		return rid;
+	}
+	public void setRid(int rid) {
+		this.rid = rid;
+	}
+	public String getReceiverAddress() {
+		return receiverAddress;
+	}
+	public void setReceiverAddress(String receiverAddress) {
+		this.receiverAddress = receiverAddress;
+	}
+	
+	
+}
