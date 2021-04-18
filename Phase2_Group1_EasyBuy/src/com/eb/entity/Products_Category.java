19a2bdc787ae86a9447f06a0cc373efa6921dc50
ztulnnx <jiabao52002443@163.com>
2021-04-18 12:08:08 +0800

    update

---
+++ Phase2_Group1_EasyBuy/src/com/eb/entity/Products_Category.java
@@ -0,0 +1,34 @@
+package com.eb.entity;
+
+public class Products_Category {
+   //品类编号
+   private String cid;
+   //品类名称
+   private String categoryName;
+   //父级品类编号
+   private String parent_category_id;
+public String getCid() {
+	return cid;
+}
+public void setCid(String cid) {
+	this.cid = cid;
+}
+public String getCategoryName() {
+	return categoryName;
+}
+public void setCategoryName(String categoryName) {
+	this.categoryName = categoryName;
+}
+public String getParent_category_id() {
+	return parent_category_id;
+}
+public void setParent_category_id(String parent_category_id) {
+	this.parent_category_id = parent_category_id;
+}
+@Override
+public String toString() {
+	return "Products_Category [cid=" + cid + ", categoryName=" + categoryName + ", parent_category_id="
+			+ parent_category_id + "]";
+}
+   
+}
---
+++ Phase2_Group1_EasyBuy/src/com/eb/entity/ReceiverAddress.java
@@ -0,0 +1,28 @@
+package com.eb.entity;
+
+public class ReceiverAddress {
+	private int rid;
+	private String  receiverAddress;
+	private int uid;
+	
+	public int getRid() {
+		return rid;
+	}
+	public void setRid(int rid) {
+		this.rid = rid;
+	}
+	public String getReceiverAddress() {
+		return receiverAddress;
+	}
+	public void setReceiverAddress(String receiverAddress) {
+		this.receiverAddress = receiverAddress;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	
+	
+}
